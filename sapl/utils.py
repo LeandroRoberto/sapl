2ef18cff9f031fbf0b1d2cd16bfe05d8941db93d
Marcio Mazza <marciomazza@gmail.com>
2017-04-10 17:50:14 -0300

    Corrige construção da view de redirecionamento

--- sapl/utils.py
+++ sapl/utils.py
@@ -652,15 +652,24 @@ def delete_texto(sender, instance, **kwargs):
     update_index = UpdateIndexCommand()
     update_index.start()
 
+def build_redirect_view(nome, lista_pk_antiga):
+
+    def view(request):
+        args = [request.GET[k] for k in lista_pk_antiga]
+        return redirect(nome, *args)
+
+    return view
+
+
 @listify
 def redirecionamento_urls_antigas(namespace, *antigos_para_novos):
     for url_antiga, destino in antigos_para_novos:
+
         partes = urlparse(url_antiga)
         regex = '^{}$'.format(partes.path)
-        pk_antiga = parse_qs(partes.query).keys()
+        lista_pk_antiga = parse_qs(partes.query).keys()
 
-        def view_redirecionamento(request):
-            args = [request.GET[k] for k in pk_antiga]
-            return redirect('{}:{}'.format(namespace, destino), *args)
+        nome = '{}:{}'.format(namespace, destino) if namespace else destino
+        view = build_redirect_view(nome, lista_pk_antiga)
 
-        yield url(regex, view_redirecionamento)
+        yield url(regex, view)
